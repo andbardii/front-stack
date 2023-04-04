8fb7c1a5f02d62430780172e46ade68f15b8a7a3
andbardii <andbardii@icloud.com>
2023-04-04 10:21:50 +0200

    prove g2

---
+++ m1/s2/g2/prove/js/script.js
@@ -0,0 +1,16 @@
+var a = 3
+var b = 7
+var c = a + b
+
+console.log(c)
+
+var b = 15
+var c = a + b
+
+console.log(c)
+
+// Uso del BACKTICK (option + 9)
+var nome = 'Andrea'
+var anni = '20'
+
+console.log(`Ciao, mi chiamo ${nome} ed ho ${anni} anni`)
