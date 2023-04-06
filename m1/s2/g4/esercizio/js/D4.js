cd4324cc7408eced5b6ce5656b248658fea7549e
andbardii <andbardii@icloud.com>
2023-04-06 16:59:23 +0200

    commit

--- m1/s2/g4/esercizio/js/D4.js
+++ m1/s2/g4/esercizio/js/D4.js
@@ -105,30 +105,85 @@ const starWarsCharacters = [
 Crea una variabile chiamata "characters" e inserisci un array vuoto
 */
 
+let characters = []
+
+console.log(characters)
+
 /* ESERCIZIO 2
 Usando un for loop, cicla l'array "starWarsCharacters" ed accedi alla proprietà "name". 
 Usa il valore contenuto inserendolo nell'array creato precedentemente. 
 Come risultato dovresti ottenere qualcosa di simile: ["Luke Skywalker", "C-3PO", "R2-D2", etc..]
 */
 
+for (let i = 0; i < starWarsCharacters.length; i++){
+  characters.push(starWarsCharacters[i].name)
+}
+
+console.log(characters)
+
 /* ESERCIZIO 3
   Seguendo i passaggi precedenti crea un array chiamato "femaleCharacters" e inserisci un oggetto con questa struttura: 
   {name: Leia Organa, hair_color: "brown", eye_color: "brown"}
 */
+
+let femaleCharacters = []
+
+femaleCharacters.push ({
+  name: "Leia Organa", 
+  hair_color: "brown", 
+  eye_color: "brown"})
+
+console.log(femaleCharacters)
+
 
 /* ESERCIZIO 4
   Crea un oggetto "eyeColor" che abbia come proprietà: blue, yellow, brown, red, blue-gray.
   ognuna di queste proprietà contiene un array vuoto
 */
 
+let eyeColor = {
+  blue: [], 
+  yellow: [],
+  brown: [],
+  red: [],
+  "blue-gray": []
+}
+
 /* ESERCIZIO 5
   Inserisci l'oggetto dei personaggi in "starWarsCharacters" nell'array corrispondente al colore dei loro occhi nell'oggetto "eyeColor" precedentemente creato
   Utilizza uno switch statement per determinare in quale proprietà inserire il personaggio
   */
+  for (let x of starWarsCharacters)
+  {
+     switch (x.eye_color) {
+       case 'blue':
+         eyeColor.blue.push(x);
+         break;
+       case 'brown':
+         eyeColor.brown.push(x);
+         break;
+       case 'yellow':
+         eyeColor.yellow.push(x);
+         break;
+       default:
+         break;
+     }
+   }
 
+   console.log(eyeColor)
 /* ESERCIZIO 6
   Usa un while loop per calcolare la massa totale dell'equipaggio
   */
+
+let massaTotale = 0
+let y = 0
+
+while(y < starWarsCharacters.length){
+  massaTotale += Number(starWarsCharacters[y].mass);
+  y++;
+}
+
+console.log(massaTotale)
 
 /* ESERCIZIO 7
 
@@ -144,9 +199,36 @@ Se la massa è superiore a 1000 stampa in console: "DANGER! OVERLOAD ALERT: Jump
 Una volta fatto, modifica la massa di qualche elemento dell'equipaggio e vedi se riesci ad ottenere un messaggio diverso.
 */
 
+switch (massaTotale) {
+  case massaTotale < 500:
+    console.log("Ship is under loaded")
+    break;
+  case massaTotale >= 500:
+    console.log("Ship is half loaded")
+    break;
+  case massaTotale >= 700:
+    console.log("Warning: Load is over 700")
+    break;
+  case massaTotale >= 900:
+    console.log("Critical Load: Over 900")
+    break;
+  case massaTotale >= 1000:
+    console.log("DANGER! OVERLOAD ALERT: Jump ship now!")
+    break;
+  default:
+    console.log("IL PROGRAMMA NON RISPONDE")
+}
+
 /* ESERCIZIO 8
 Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi creare un nuovo array, o tentare la riassegnazione del valore corrispondente)
 */
+
+for (let i = 0; i < starWarsCharacters.length; i++){
+  if (starWarsCharacters[i].gender === "n/a"){
+    starWarsCharacters[i].gender = "robot"
+  }
+}
+console.log(starWarsCharacters)
 
 /* EXTRA ESERCIZIO 9
 
@@ -158,6 +240,18 @@ Usa uno più for loop per raggiungere il risultato
 Una volta fatto crea un conosle.log per controllare la proprietà length di "characters" prima e dopo l'operazione
 */
 
+for (let i = 0; i < femaleCharacters.length; i++){
+  if (characters[i] == femaleCharacters[i]){
+    characters.pop()
+}
+}
+
+console.log(characters)
+
 /* EXTRA ESERCIZIO 10
 Crea una funzionalità che prenda un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo
 */
+
+function printProperties(element){
+  console.log(element.name + " " + element.height + " " + element.mass + " " + element.hair_color + " " + element.skin_color + " " + element.eye_color + " " + element.birth_year + " " + element.gender)
+}
