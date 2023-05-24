87f432e7a4d704a154314efbd276bb1a6b3c05ec
andbardii <andbardii@icloud.com>
2023-05-24 16:42:50 +0200

    add

--- m3/s1/g3/script.js
+++ m3/s1/g3/script.js
@@ -15,11 +15,12 @@ var __extends = (this && this.__extends) || (function () {
 })();
 //* CLASSE ASTRATTA
 var LavoratoreAutonomo = /** @class */ (function () {
-    function LavoratoreAutonomo(codRedd, redditoAnnuoLordo, tasseInps, tasseIrpef) {
+    function LavoratoreAutonomo(codRedd, redditoAnnuoLordo, tasseInps, tasseIrpef, professione) {
         this.codRedd = codRedd;
         this.redditoAnnuoLordo = redditoAnnuoLordo;
         this.tasseInps = tasseInps;
         this.tasseIrpef = tasseIrpef;
+        this.professione = professione;
     }
     return LavoratoreAutonomo;
 }());
@@ -27,9 +28,7 @@ var LavoratoreAutonomo = /** @class */ (function () {
 var MuratoreAutonomo = /** @class */ (function (_super) {
     __extends(MuratoreAutonomo, _super);
     function MuratoreAutonomo(redditoAnnuoLordo) {
-        var _this = _super.call(this, 0.3, redditoAnnuoLordo, 0.1, 0.12) || this;
-        _this.redditoAnnuoLordo = redditoAnnuoLordo;
-        return _this;
+        return _super.call(this, 0.3, redditoAnnuoLordo, 0.1, 0.12, "Muratore") || this;
     }
     MuratoreAutonomo.prototype.getUtileTasse = function () {
         return this.redditoAnnuoLordo * this.codRedd;
@@ -41,14 +40,47 @@ var MuratoreAutonomo = /** @class */ (function (_super) {
         return this.getUtileTasse() * this.tasseIrpef;
     };
     MuratoreAutonomo.prototype.getRedditoAnnuoNetto = function () {
-        var redditoNetto = this.redditoAnnuoLordo - (this.getTasselnps() + this.getTasselrpef());
+        var redditoNetto = this.redditoAnnuoLordo - this.getTasselnps() - this.getTasselrpef();
         console.log("IL TUO REDDITO NETTO:");
         console.log(redditoNetto + "$");
     };
     return MuratoreAutonomo;
 }(LavoratoreAutonomo));
+//* CLASSE INFORMATICO 
+var InformaticoAutonomo = /** @class */ (function (_super) {
+    __extends(InformaticoAutonomo, _super);
+    function InformaticoAutonomo(redditoAnnuoLordo) {
+        var _this = _super.call(this, 0.75, redditoAnnuoLordo, 0.15, 0.16, "Informatico") || this;
+        _this.redditoAnnuoLordo = redditoAnnuoLordo;
+        return _this;
+    }
+    InformaticoAutonomo.prototype.getUtileTasse = function () {
+        return this.redditoAnnuoLordo * this.codRedd;
+    };
+    InformaticoAutonomo.prototype.getTasselnps = function () {
+        return this.getUtileTasse() * this.tasseInps;
+    };
+    InformaticoAutonomo.prototype.getTasselrpef = function () {
+        return this.getUtileTasse() * this.tasseIrpef;
+    };
+    InformaticoAutonomo.prototype.getRedditoAnnuoNetto = function () {
+        var redditoNetto = this.redditoAnnuoLordo - this.getTasselnps() - this.getTasselrpef();
+        console.log("IL TUO REDDITO NETTO:");
+        console.log(redditoNetto + "$");
+    };
+    return InformaticoAutonomo;
+}(LavoratoreAutonomo));
 // NEW MURATORE AUTONOMO
 var AlfredoMuratoreAutonomo = new MuratoreAutonomo(90000);
+console.log("Data la professione di: " + AlfredoMuratoreAutonomo.professione);
+console.log("Con un reddito lordo di: " + AlfredoMuratoreAutonomo.redditoAnnuoLordo);
 AlfredoMuratoreAutonomo.getRedditoAnnuoNetto();
 console.log("Hai pagato all'INPS: " + AlfredoMuratoreAutonomo.getTasselnps() + "$");
 console.log("Hai pagato per l'IRPEF: " + AlfredoMuratoreAutonomo.getTasselrpef() + "$");
+// NEW INFORMATICO AUTONOMO
+var GiovanniInformaticoAutonomo = new InformaticoAutonomo(70000);
+console.log("Data la professione di: " + GiovanniInformaticoAutonomo.professione);
+console.log("Con un reddito lordo di: " + GiovanniInformaticoAutonomo.redditoAnnuoLordo);
+GiovanniInformaticoAutonomo.getRedditoAnnuoNetto();
+console.log("Hai pagato all'INPS: " + GiovanniInformaticoAutonomo.getTasselnps() + "$");
+console.log("Hai pagato per l'IRPEF: " + GiovanniInformaticoAutonomo.getTasselrpef() + "$");
--- m3/s1/g3/script.ts
+++ m3/s1/g3/script.ts
@@ -6,11 +6,14 @@ abstract class LavoratoreAutonomo {
     tasseInps: number;
     tasseIrpef: number;
 
-    constructor(codRedd: number, redditoAnnuoLordo: number, tasseInps: number, tasseIrpef: number){
+    professione: string;
+
+    constructor(codRedd: number, redditoAnnuoLordo: number, tasseInps: number, tasseIrpef: number, professione: string) {
         this.codRedd = codRedd;
         this.redditoAnnuoLordo = redditoAnnuoLordo;
         this.tasseInps = tasseInps;
         this.tasseIrpef = tasseIrpef;
+        this.professione = professione;
     }
 
     abstract getUtileTasse(): number;
@@ -25,7 +28,32 @@ class MuratoreAutonomo extends LavoratoreAutonomo{
 
 
     constructor(redditoAnnuoLordo:number){
-        super(0.3, redditoAnnuoLordo, 0.1, 0.12 );
+        super(0.3, redditoAnnuoLordo, 0.1, 0.12, "Muratore");
+    }
+
+    getUtileTasse(): number {
+        return this.redditoAnnuoLordo * this.codRedd
+    }
+    getTasselnps(): number {
+        return this.getUtileTasse() * this.tasseInps
+    }
+    getTasselrpef(): number {
+        return this.getUtileTasse() * this.tasseIrpef
+    }
+    getRedditoAnnuoNetto(): void {
+        let redditoNetto = this.redditoAnnuoLordo - this.getTasselnps() - this.getTasselrpef()
+        console.log("IL TUO REDDITO NETTO:")
+        console.log(redditoNetto + "$")
+    }
+
+}
+
+//* CLASSE INFORMATICO 
+class InformaticoAutonomo extends LavoratoreAutonomo{
+
+
+    constructor(redditoAnnuoLordo:number){
+        super(0.75, redditoAnnuoLordo, 0.15, 0.16, "Informatico");
         this.redditoAnnuoLordo = redditoAnnuoLordo;
     }
 
@@ -39,17 +67,30 @@ class MuratoreAutonomo extends LavoratoreAutonomo{
         return this.getUtileTasse() * this.tasseIrpef
     }
     getRedditoAnnuoNetto(): void {
-        let redditoNetto = this.redditoAnnuoLordo - (this.getTasselnps() + this.getTasselrpef())
+        let redditoNetto = this.redditoAnnuoLordo - this.getTasselnps() - this.getTasselrpef()
         console.log("IL TUO REDDITO NETTO:")
         console.log(redditoNetto + "$")
     }
 
 }
 
+
 // NEW MURATORE AUTONOMO
 let AlfredoMuratoreAutonomo = new MuratoreAutonomo(90000)
- 
+console.log("Data la professione di: " + AlfredoMuratoreAutonomo.professione)
+console.log("Con un reddito lordo di: " + AlfredoMuratoreAutonomo.redditoAnnuoLordo)
+
 AlfredoMuratoreAutonomo.getRedditoAnnuoNetto()
 
 console.log("Hai pagato all'INPS: " + AlfredoMuratoreAutonomo.getTasselnps() + "$")
 console.log("Hai pagato per l'IRPEF: " + AlfredoMuratoreAutonomo.getTasselrpef() + "$")
+
+// NEW INFORMATICO AUTONOMO
+let GiovanniInformaticoAutonomo = new InformaticoAutonomo(70000)
+console.log("Data la professione di: " + GiovanniInformaticoAutonomo.professione)
+console.log("Con un reddito lordo di: " + GiovanniInformaticoAutonomo.redditoAnnuoLordo)
+
+GiovanniInformaticoAutonomo.getRedditoAnnuoNetto()
+
+console.log("Hai pagato all'INPS: " + GiovanniInformaticoAutonomo.getTasselnps() + "$")
+console.log("Hai pagato per l'IRPEF: " + GiovanniInformaticoAutonomo.getTasselrpef() + "$")
