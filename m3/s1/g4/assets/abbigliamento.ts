72a14c6bf074490b69e2d602ab6ab421e4704aa9
andbardii <andbardii@icloud.com>
2023-05-25 16:17:45 +0200

    add

--- m3/s1/g4/assets/abbigliamento.ts
+++ m3/s1/g4/assets/abbigliamento.ts
@@ -1,4 +1,5 @@
-class capo{
+// CLASSE SINGOLO CAPO
+class Capo{
 
     id:number
     codprod:number
@@ -7,12 +8,12 @@ class capo{
     modello:number 
     quantita:number
     colore:string
-    prezzoIvaEsclusa:number
-    prezzoIvaInclusa:number
+    prezzoivaesclusa:number
+    prezzoivainclusa:number
     disponibile:string
     saldo:number
 
-    constructor(id:number,codprod:number,collezione:string,capo:string,modello:number,quantita:number,colore:string,prezzoIvaEsclusa:number,prezzoIvaInclusa:number,disponibile:string,saldo:number){
+    constructor(id:number,codprod:number,collezione:string,capo:string,modello:number,quantita:number,colore:string,prezzoivaesclusa:number,prezzoivainclusa:number,disponibile:string,saldo:number){
         this.id = id;
         this.codprod = codprod;
         this.collezione = collezione;
@@ -20,18 +21,62 @@ class capo{
         this.modello = modello;
         this.quantita = quantita;
         this.colore = colore;
-        this.prezzoIvaEsclusa = prezzoIvaEsclusa; 
-        this.prezzoIvaInclusa = prezzoIvaInclusa;
+        this.prezzoivaesclusa = prezzoivaesclusa; 
+        this.prezzoivainclusa = prezzoivainclusa;
         this.disponibile = disponibile;
         this.saldo = saldo;
+
+        this.mostraPrezzo()
     }
 
-    getPrezzoCapo():number {
-        throw new Error("Not implemented. (getPrezzoCapo())")
-    }
-    
-    getAcquistoCapo():number{
-        throw new Error("Not implemented. (getAcquistoCapo())")
+    mostraPrezzo():void{ 
+        console.log('%cMOSTRA PRODOTTO', 'color: orange');
+        console.log("NOME: " + this.capo)
+        console.log("Prezzo con IVA: %c" + + this.prezzoivainclusa + "$", 'color: red')
     }
 
+}
+
+// FETCH GENERALE CAPI
+fetch("assets/Abbigliamento.json")
+.then((res:Response)=> {
+    if(res.ok){
+        return res.json()
+    }else{
+        throw new Error("ERRORE NEL CARICAMENTO DELLA FETCH")
+    }
+})
+.then((res) => {
+    console.warn("ARRAY DELLA FETCH")
+    console.log(res)
+    handlePage(res)
+})
+.catch((err)=>{
+    console.error(err)
+})
+
+
+
+const handlePage = function(capi:[]){
+    let nuovoArray:any = []
+    capi.forEach((element:any) => {
+        let nuovoCapo = new Capo
+        (element.id,
+        element.codprod,
+        element.collezione,
+        element.capo,
+        element.modello,
+        element.quantita,
+        element.colore,
+        element.prezzoivaesclusa,
+        element.prezzoivainclusa,
+        element.disponibile,
+        element.saldo)
+
+        nuovoArray.push(nuovoCapo)
+        console.log(nuovoCapo)
+    })
+
+    console.warn("NUOVO ARRAY TIPIZZATO")
+    console.log(nuovoArray)
 }
--- m3/s1/g4/assets/script/abbigliamento.js
+++ m3/s1/g4/assets/script/abbigliamento.js
@@ -1,5 +1,6 @@
-var capo = /** @class */ (function () {
-    function capo(id, codprod, collezione, capo, modello, quantita, colore, prezzoIvaEsclusa, prezzoIvaInclusa, disponibile, saldo) {
+// CLASSE SINGOLO CAPO
+var Capo = /** @class */ (function () {
+    function Capo(id, codprod, collezione, capo, modello, quantita, colore, prezzoivaesclusa, prezzoivainclusa, disponibile, saldo) {
         this.id = id;
         this.codprod = codprod;
         this.collezione = collezione;
@@ -7,16 +8,44 @@ var capo = /** @class */ (function () {
         this.modello = modello;
         this.quantita = quantita;
         this.colore = colore;
-        this.prezzoIvaEsclusa = prezzoIvaEsclusa;
-        this.prezzoIvaInclusa = prezzoIvaInclusa;
+        this.prezzoivaesclusa = prezzoivaesclusa;
+        this.prezzoivainclusa = prezzoivainclusa;
         this.disponibile = disponibile;
         this.saldo = saldo;
+        this.mostraPrezzo();
     }
-    capo.prototype.getPrezzoCapo = function () {
-        throw new Error("Not implemented. (getPrezzoCapo())");
+    Capo.prototype.mostraPrezzo = function () {
+        console.log('%cMOSTRA PRODOTTO', 'color: orange');
+        console.log("NOME: " + this.capo);
+        console.log("Prezzo con IVA: %c" + +this.prezzoivainclusa + "$", 'color: red');
     };
-    capo.prototype.getAcquistoCapo = function () {
-        throw new Error("Not implemented. (getAcquistoCapo())");
-    };
-    return capo;
+    return Capo;
 }());
+// FETCH GENERALE CAPI
+fetch("assets/Abbigliamento.json")
+    .then(function (res) {
+    if (res.ok) {
+        return res.json();
+    }
+    else {
+        throw new Error("ERRORE NEL CARICAMENTO DELLA FETCH");
+    }
+})
+    .then(function (res) {
+    console.warn("ARRAY DELLA FETCH");
+    console.log(res);
+    handlePage(res);
+})
+    .catch(function (err) {
+    console.error(err);
+});
+var handlePage = function (capi) {
+    var nuovoArray = [];
+    capi.forEach(function (element) {
+        var nuovoCapo = new Capo(element.id, element.codprod, element.collezione, element.capo, element.modello, element.quantita, element.colore, element.prezzoivaesclusa, element.prezzoivainclusa, element.disponibile, element.saldo);
+        nuovoArray.push(nuovoCapo);
+        console.log(nuovoCapo);
+    });
+    console.warn("NUOVO ARRAY TIPIZZATO");
+    console.log(nuovoArray);
+};
