06f47bd5660a90de55b26f51292197801c081074
andbardii <andbardii@icloud.com>
2023-05-26 16:51:05 +0200

    mamma mia che robba

--- m3/s1/g5/script.js
+++ m3/s1/g5/script.js
@@ -55,6 +55,8 @@ var Smartphone = /** @class */ (function () {
             console.log("%cATTENZIONE CREDITO AZZERATO!", "color: red");
             this.carica -= costoChiamata;
             this.numeroChiamate++;
+            var now = new Date();
+            this.registroChiamate.push(new Chiamata(this.numeroChiamate, min, now));
         }
         else if (costoChiamata > this.carica) {
             console.log("%cATTENZIONE CREDITO INSUFFICIENTE!", "color: red");
@@ -66,26 +68,49 @@ var Smartphone = /** @class */ (function () {
     };
     return Smartphone;
 }());
+//! GESTIONE CONSOLE 
 console.warn("IPHONE");
 var IPhone = new Smartphone(0, 0);
 IPhone.ricarica(50);
-IPhone.chiamata(10);
-IPhone.chiamata(10);
-console.log(IPhone.getNumeroChiamate());
-IPhone.mostraRegistroChiamate();
-IPhone.filtraChiamatePerDataOra(26, 13);
-IPhone.filtraChiamatePerDataOra(26, 12);
+// IPhone.chiamata(10)
+// IPhone.chiamata(10)
+// console.log(IPhone.getNumeroChiamate())
+// IPhone.mostraRegistroChiamate()
+// IPhone.filtraChiamatePerDataOra(26, 13)
+// IPhone.filtraChiamatePerDataOra(26, 12)
 console.warn("SAMSUNG");
 var Samsung = new Smartphone(0, 0);
-Samsung.ricarica(60);
-Samsung.chiamata(500);
-Samsung.chiamata(50);
-Samsung.chiamata(50);
-console.log(Samsung.numero404());
-console.log(Samsung.getNumeroChiamate());
-Samsung.azzeraChiamate();
-console.log(Samsung.getNumeroChiamate());
-Samsung.mostraRegistroChiamate();
+// Samsung.ricarica(60)
+// Samsung.chiamata(500)
+// Samsung.chiamata(50)
+// Samsung.chiamata(50)
+// console.log(Samsung.numero404())
+// console.log(Samsung.getNumeroChiamate())
+// Samsung.azzeraChiamate()
+// console.log(Samsung.getNumeroChiamate())
+// Samsung.mostraRegistroChiamate()
 console.warn("XIAOMI");
 var Xiaomi = new Smartphone(0, 0);
-Xiaomi.chiamata(10);
+// Xiaomi.chiamata(10)
+//! GESTIONE INDEX
+var target = document.querySelector("#screen");
+var phoneApp = document.querySelector("#gestione-smartphone");
+phoneApp === null || phoneApp === void 0 ? void 0 : phoneApp.addEventListener("click", function () {
+    target.innerHTML = "";
+    var insidePhoneApp = document.createElement("div");
+    insidePhoneApp.classList.add("container-fluid", "h-100");
+    insidePhoneApp.innerHTML = "<div class=\"row p-2 h-50\">\n                                <div class=\"col-12 fw-bold\">".concat(IPhone.numero404(), "\n                                <div class=\"col-12 btn btn-success\" id=\"btn-ricarica\">RICARICA</div>\n                                <div class=\"col-12 btn btn-danger\">CANCELLA CHIAMATE</div>\n                                </div>\n                                <div class=\"col-12 fw-bold text-secondary\"><span>CHIAMATE EFFETTUATE: </span>").concat(IPhone.getNumeroChiamate(), "</div>\n                                <div class=\"col-12\">\n                                <div class=\"row overflow-auto\" id=\"chiamate\">\n\n                                </div>\n                                </div>\n                                <div class=\"col-12 text-center generic-app\" id=\"bottone-chiama\">\n                                    <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"35\" height=\"35\" fill=\"green\" class=\"bi bi-telephone-fill\" viewBox=\"0 0 16 16\">\n                                        <path fill-rule=\"evenodd\" d=\"M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z\"/>\n                                </svg>\n                                </div>\n                                </div>\n                                ");
+    target.appendChild(insidePhoneApp);
+    var bottoneChiama = insidePhoneApp.querySelector("#bottone-chiama");
+    var targetChiamate = insidePhoneApp.querySelector("#chiamate");
+    bottoneChiama === null || bottoneChiama === void 0 ? void 0 : bottoneChiama.addEventListener("click", function () {
+        IPhone.chiamata(Math.floor(Math.random() * 50));
+        targetChiamate.innerHTML = "";
+        IPhone.registroChiamate.forEach(function (call) {
+            var dChiamata = document.createElement("div");
+            dChiamata.classList.add("col-12", "border-bottom", "border-secondary");
+            dChiamata.innerHTML = "<span class=\"me-2\">".concat(call.id, "</span> <span class=\"me-2\">").concat(call.durata, "</span class=\"me-2\"> <span>").concat(call.data, "</span>");
+            targetChiamate.appendChild(dChiamata);
+        });
+    });
+});
--- m3/s1/g5/script.ts
+++ m3/s1/g5/script.ts
@@ -80,6 +80,8 @@ class Smartphone implements ISmartphone{
 
             this.carica -= costoChiamata
             this.numeroChiamate ++
+            let now:Date = new Date()
+            this.registroChiamate.push(new Chiamata(this.numeroChiamate, min, now))
         }else if(costoChiamata > this.carica){
             console.log("%cATTENZIONE CREDITO INSUFFICIENTE!","color: red")
         }
@@ -92,29 +94,72 @@ class Smartphone implements ISmartphone{
 
 }
 
+//! GESTIONE CONSOLE 
 console.warn("IPHONE")
 let IPhone = new Smartphone(0, 0)
 IPhone.ricarica(50)
-IPhone.chiamata(10)
-IPhone.chiamata(10)
-console.log(IPhone.getNumeroChiamate())
-IPhone.mostraRegistroChiamate()
-IPhone.filtraChiamatePerDataOra(26, 13)
-IPhone.filtraChiamatePerDataOra(26, 12)
+// IPhone.chiamata(10)
+// IPhone.chiamata(10)
+// console.log(IPhone.getNumeroChiamate())
+// IPhone.mostraRegistroChiamate()
+// IPhone.filtraChiamatePerDataOra(26, 13)
+// IPhone.filtraChiamatePerDataOra(26, 12)
 
 console.warn("SAMSUNG")
 let Samsung = new Smartphone(0, 0)
-Samsung.ricarica(60)
-Samsung.chiamata(500)
-Samsung.chiamata(50)
-Samsung.chiamata(50)
+// Samsung.ricarica(60)
+// Samsung.chiamata(500)
+// Samsung.chiamata(50)
+// Samsung.chiamata(50)
 
-console.log(Samsung.numero404())
-console.log(Samsung.getNumeroChiamate())
-Samsung.azzeraChiamate()
-console.log(Samsung.getNumeroChiamate())
-Samsung.mostraRegistroChiamate()
+// console.log(Samsung.numero404())
+// console.log(Samsung.getNumeroChiamate())
+// Samsung.azzeraChiamate()
+// console.log(Samsung.getNumeroChiamate())
+// Samsung.mostraRegistroChiamate()
 
 console.warn("XIAOMI")
 let Xiaomi = new Smartphone(0, 0)
-Xiaomi.chiamata(10)
+// Xiaomi.chiamata(10)
+
+//! GESTIONE INDEX
+let target:HTMLElement|null = document.querySelector("#screen")
+let phoneApp:HTMLElement|null = document.querySelector("#gestione-smartphone")
+phoneApp?.addEventListener("click", () =>{
+    target!.innerHTML = ""
+    let insidePhoneApp:HTMLElement = document.createElement("div")
+    insidePhoneApp.classList.add("container-fluid", "h-100")
+    insidePhoneApp.innerHTML = `<div class="row p-2 h-50">
+                                <div class="col-12 fw-bold">${IPhone.numero404()}
+                                <div class="col-12 btn btn-success" id="btn-ricarica">RICARICA</div>
+                                <div class="col-12 btn btn-danger">CANCELLA CHIAMATE</div>
+                                </div>
+                                <div class="col-12 fw-bold text-secondary"><span>CHIAMATE EFFETTUATE: </span>${IPhone.getNumeroChiamate()}</div>
+                                <div class="col-12">
+                                <div class="row overflow-auto" id="chiamate">
+
+                                </div>
+                                </div>
+                                <div class="col-12 text-center generic-app" id="bottone-chiama">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="green" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                                </svg>
+                                </div>
+                                </div>
+                                `
+    target!.appendChild(insidePhoneApp)
+
+let bottoneChiama:HTMLElement|null = insidePhoneApp.querySelector("#bottone-chiama")
+let targetChiamate:HTMLElement|null = insidePhoneApp.querySelector("#chiamate")
+bottoneChiama?.addEventListener("click", () => {
+
+    IPhone.chiamata(Math.floor(Math.random() * 50))
+    targetChiamate!.innerHTML = ""
+    IPhone.registroChiamate.forEach((call:Chiamata)=>{
+        let dChiamata:HTMLElement = document.createElement("div")
+        dChiamata.classList.add("col-12", "border-bottom", "border-secondary")
+        dChiamata.innerHTML =`<span class="me-2">${call.id}</span> <span class="me-2">${call.durata}</span class="me-2"> <span>${call.data}</span>`
+        targetChiamate!.appendChild(dChiamata)
+    })
+})
+})
