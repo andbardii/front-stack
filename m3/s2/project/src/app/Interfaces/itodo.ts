e1466483cd26f05175ff87f929a34106aec85582
andbardii <andbardii@icloud.com>
2023-06-02 10:49:33 +0200

    basic working

--- m3/s2/project/src/app/Interfaces/itodo.ts
+++ m3/s2/project/src/app/Interfaces/itodo.ts
@@ -1,2 +1,6 @@
 export interface Itodo {
+  id:number;
+  title:string;
+  completed:boolean;
 }
+
--- m3/s2/project/src/app/app-routing.module.ts
+++ m3/s2/project/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompletedComponent } from './Pages/completed/completed.component';
+import { TodoComponent } from './Pages/todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoComponent,
+  },
+  {
+    path: 'completed',
+    component: CompletedComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
