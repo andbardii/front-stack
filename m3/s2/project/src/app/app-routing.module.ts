87da45dfccb3712c43dd10e23ef3bf639bfa9a13
andbardii <andbardii@icloud.com>
2023-06-03 12:46:23 +0200

    add UI

--- m3/s2/project/src/app/app-routing.module.ts
+++ m3/s2/project/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompletedComponent } from './Pages/completed/completed.component';
 import { TodoComponent } from './Pages/todo/todo.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'todo',
     component: TodoComponent,
   },
   {
--- m3/s2/project/src/app/app.module.ts
+++ m3/s2/project/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TodoComponent } from './Pages/todo/todo.component';
 import { CompletedComponent } from './Pages/completed/completed.component';
 import { NavbarComponent } from './Elements/navbar/navbar.component';
 import { FooterComponent } from './Elements/footer/footer.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './Elements/footer/footer.component';
     TodoComponent,
     CompletedComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
     ],
   imports: [
     BrowserModule,
