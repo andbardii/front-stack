5879bfff26f5677ea76ba5abf9e34c08a714f84a
andbardii <andbardii@icloud.com>
2023-06-05 18:21:46 +0200

    add

--- m3/s3/g1/src/app/Pages/liked/liked.component.ts
+++ m3/s3/g1/src/app/Pages/liked/liked.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cphoto } from 'src/app/Model/cphoto';
+import { SphotoService } from 'src/app/Services/sphoto.service';
 
 @Component({
   selector: 'app-liked',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./liked.component.scss']
 })
 export class LikedComponent {
+  likedArr:Cphoto[] = [];
 
+  constructor(private photoSvc:SphotoService){}
+
+  ngOnInit() {
+    this.getLiked()
+  }
+
+  getLiked(){
+    this.photoSvc.getPhotos().subscribe(result => {
+      this.likedArr = result.filter(photo => photo.liked == true);
+    })
+  }
+
+  delete(id?:number){
+    this.photoSvc.deletePhoto(id!).subscribe(result => {
+      this.getLiked()
+    })
+  }
+
+  like(photo:Cphoto) {
+    this.photoSvc.likeThis(photo).subscribe(result => {
+      this.getLiked()
+    })
+  }
 }
