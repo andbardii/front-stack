484e303c903541d49b227391262cbcaf7d53b621
andbardii <andbardii@icloud.com>
2023-06-06 16:18:28 +0200

    add driven

--- m3/s3/g2/src/app/app.module.ts
+++ m3/s3/g2/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DrivenComponent } from './Pages/driven/driven.component';
+import { ReactiveComponent } from './Pages/reactive/reactive.component';
+import { NavbarComponent } from './Elements/navbar/navbar.component';
+import { FooterComponent } from './Elements/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrivenComponent,
+    ReactiveComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
